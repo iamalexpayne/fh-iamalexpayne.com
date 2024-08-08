3947622d6dab722b29cc7f5c94abdea482771d27
Alex Payne <alexpayne@MacBook-Pro.local>
2024-08-08 16:53:23 -0400

    test 3 for svelte

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { base } from "$app/paths";
+</script>
+
+<h1>Home</h1>
+<p>This is the home page of Alex</p>
+<a href="{base}/about">About</a>
---
+++ website/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { base } from "$app/paths";
+</script>
+
+<h1>About</h1>
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa tenetur,
+    numquam corrupti iure qui animi illo quaerat aperiam atque culpa, ut debitis
+    dolore sit autem reprehenderit dolorum aliquid dignissimos nemo?
+</p>
+<a href="{base}/">Home</a>
