3ec65f2de39e667b1b114627fd28141cbaaf4e2e
Alex Payne <alexpayne@MacBook-Pro.local>
2024-08-09 10:53:09 -0400

    test 6

---
+++ website/docs/_app/immutable/entry/start.BkHpCdZo.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Ck8stwNi.js";export{t as start};
---
+++ website/docs/_app/immutable/nodes/4.C8Mg334e.js
@@ -0,0 +1,3 @@
+import{f as s,s as a,a as m,t as u}from"../chunks/disclose-version.FPEv_wiP.js";import{s as p}from"../chunks/attributes.B8sQeY0B.js";import{b as n}from"../chunks/paths.DQcPSu1N.js";var l=u(`<h1>About</h1> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa tenetur,
+    numquam corrupti iure qui animi illo quaerat aperiam atque culpa, ut debitis
+    dolore sit autem reprehenderit dolorum aliquid dignissimos nemo?</p> <a>Home</a>`,1);function b(e){var t=l(),r=s(t),i=a(a(r,!0)),o=a(a(i,!0));p(o,"href",`${n??""}/`),m(e,t)}export{b as component};
