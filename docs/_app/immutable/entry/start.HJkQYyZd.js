a57a48d8430733a298ec3f42fb4e2ba03f372eb6
Alex Payne <alexpayne@MacBook-Pro.local>
2024-08-09 18:03:09 -0400

    fixed scroll bug

---
+++ docs/_app/immutable/entry/start.HJkQYyZd.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BemUzqBl.js";export{t as start};
--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import "../app.css";
 </script>
 
-<div class="bg-beige flex-grow p-8">
+<div class="bg-beige flex-grow px-8">
     <slot />
 </div>
